26515c6e847d08e5db0ced1e2d06bffd5129c171
Haroon Awan <39236904+haroonawanethicalhacker@users.noreply.github.com>
2019-03-30 06:55:37 +0500

    Maahro

--- maahro.pl
+++ maahro.pl
@@ -1,3 +1,12 @@
+#***************************************************************************************#
+#----------------------- EULA LICENSE AGREEMENT NOTICE ---------------------------------#
+#1. This software uses EULA based software agreement that grants users rights to use for#
+#any purpose, modify and redistribute creative works about this perl software.          #
+#2. This software comes in "is-as" warranty, author "Haroon Awan" take no responsbility #
+#what you do with by/this software. Your free to use this software as it is for any     #
+#purpose that suites as long as it is not related to crime.                             #
+#***************************************************************************************#
+
 #!/usr/bin/env perl
 use LWP::UserAgent;
 no warnings 'uninitialized';
@@ -10,7 +19,7 @@ print "\n\n					   	 	    Project\n";
 print "\n 						          : Maahro :\n\n\n";
 print color('bold yellow');
 print "[ + ] Programmer: 	Haroon Awan\n";
-print "[ + ] License: 		GNU\n";
+print "[ + ] License: 		EULA\n";
 print "[ + ] Version: 		1.0\n";
 print "[ + ] Contact: 		mrharoonawan\@gmail\.com \n";
 print "[ + ] Environment: 	Shell & Perl under Kali\n";
