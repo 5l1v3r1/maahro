b176da5326e862a923ea6feade2de28ebdd5099e
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-03-31 13:09:34 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/perl:~/root/Downloads/maahro/
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/perl:~/root/Downloads/maahro-master/
 alias myscript=~/root/Downloads/maahro/
 cd /usr/bin
-ln -fsT /root/Downloads/maahro/maahro.pl /usr/bin/maahro
+ln -fsT /root/Downloads/maahro-master/maahro.pl /usr/bin/maahro
 chmod u+x maahro.pl
 chmod u+x cleaner.sh
 chmod u+x /usr/bin/maahro
